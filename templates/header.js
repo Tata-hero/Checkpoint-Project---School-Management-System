514b9aac685c3ee018f79f9df5bcbda38ca0ec9a
Tatiana <tata8wheel@gmail.com>
2023-11-15 17:35:26 +0100

    Removed the container in navigation part

--- templates/header.js
+++ templates/header.js
@@ -8,11 +8,9 @@ export function createLogo() {
 export function createNavigation(menuItem) {
   return `
     <nav>
-      <div class="container">
         <ul class="menu">
           ${menuItem}
         </ul>
-      </div>
     </nav>`;
 }
 
