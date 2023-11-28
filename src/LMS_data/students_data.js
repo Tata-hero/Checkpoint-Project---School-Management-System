bbe3ac716ef1304c732e633373186a14b6f4f5f2
Tatiana <tata8wheel@gmail.com>
2023-11-28 13:34:34 +0100

    Created students data

---
+++ src/LMS_data/students_data.js
@@ -0,0 +1,204 @@
+const studentsData = [
+  {
+    id: 1,
+    name: "Alice Johnson",
+    course: "Fullstack Web Programming",
+    subjects: [
+      { name: "JavaScript Fundamentals", grade: 4 },
+      { name: "HTML and CSS Basics", grade: 4 },
+      { name: "Responsive Web Design", grade: 6 },
+    ],
+  },
+  {
+    id: 2,
+    name: "Bob Smith",
+    course: "AWS Cloud Engineering",
+    subjects: [
+      { name: "Introduction to AWS Services", grade: 5 },
+      { name: "Cloud Security", grade: 4 },
+      { name: "Serverless Architecture", grade: 6 },
+    ],
+  },
+  {
+    id: 3,
+    name: "Charlie Brown",
+    course: "Software Testing",
+    subjects: [
+      { name: "Testing Methodologies", grade: 6 },
+      { name: "Automated Testing with Selenium", grade: 5 },
+      { name: "Performance Testing", grade: 5 },
+    ],
+  },
+  {
+    id: 4,
+    name: "David White",
+    course: "Fullstack Web Programming",
+    subjects: [
+      { name: "JavaScript Fundamentals", grade: 5 },
+      { name: "HTML and CSS Basics", grade: 5 },
+      { name: "Responsive Web Design", grade: 5 },
+    ],
+  },
+  {
+    id: 5,
+    name: "Eva Rodriguez",
+    course: "AWS Cloud Engineering",
+    subjects: [
+      { name: "Introduction to AWS Services", grade: 6 },
+      { name: "Cloud Security", grade: 6 },
+      { name: "Serverless Architecture", grade: 4 },
+    ],
+  },
+  {
+    id: 6,
+    name: "Frank Thomas",
+    course: "Software Testing",
+    subjects: [
+      { name: "Testing Methodologies", grade: 5 },
+      { name: "Automated Testing with Selenium", grade: 6 },
+      { name: "Performance Testing", grade: 4 },
+    ],
+  },
+  {
+    id: 7,
+    name: "Grace Lee",
+    course: "Fullstack Web Programming",
+    subjects: [
+      { name: "JavaScript Fundamentals", grade: 6 },
+      { name: "HTML and CSS Basics", grade: 5 },
+      { name: "Responsive Web Design", grade: 4 },
+    ],
+  },
+  {
+    id: 8,
+    name: "Henry Wang",
+    course: "AWS Cloud Engineering",
+    subjects: [
+      { name: "Introduction to AWS Services", grade: 4 },
+      { name: "Cloud Security", grade: 5 },
+      { name: "Serverless Architecture", grade: 4 },
+    ],
+  },
+  {
+    id: 9,
+    name: "Ivy Chen",
+    course: "Software Testing",
+    subjects: [
+      { name: "Testing Methodologies", grade: 6 },
+      { name: "Automated Testing with Selenium", grade: 5 },
+      { name: "Performance Testing", grade: 3 },
+    ],
+  },
+  {
+    id: 10,
+    name: "Jack Martin",
+    course: "Fullstack Web Programming",
+    subjects: [
+      { name: "JavaScript Fundamentals", grade: 3 },
+      { name: "HTML and CSS Basics", grade: 5 },
+      { name: "Responsive Web Design", grade: 5 },
+    ],
+  },
+  {
+    id: 11,
+    name: "Karen Anderson",
+    course: "AWS Cloud Engineering",
+    subjects: [
+      { name: "Introduction to AWS Services", grade: 5 },
+      { name: "Cloud Security", grade: 4 },
+      { name: "Serverless Architecture", grade: 6 },
+    ],
+  },
+  {
+    id: 12,
+    name: "Leo Garcia",
+    course: "Software Testing",
+    subjects: [
+      { name: "Testing Methodologies", grade: 4 },
+      { name: "Automated Testing with Selenium", grade: 4 },
+      { name: "Performance Testing", grade: 3 },
+    ],
+  },
+  {
+    id: 13,
+    name: "Mia Wilson",
+    course: "Fullstack Web Programming",
+    subjects: [
+      { name: "JavaScript Fundamentals", grade: 4 },
+      { name: "HTML and CSS Basics", grade: 5 },
+      { name: "Responsive Web Design", grade: 6 },
+    ],
+  },
+  {
+    id: 14,
+    name: "Nathan Brown",
+    course: "AWS Cloud Engineering",
+    subjects: [
+      { name: "Introduction to AWS Services", grade: 6 },
+      { name: "Cloud Security", grade: 5 },
+      { name: "Serverless Architecture", grade: 4 },
+    ],
+  },
+  {
+    id: 15,
+    name: "Olivia Taylor",
+    course: "Software Testing",
+    subjects: [
+      { name: "Testing Methodologies", grade: 5 },
+      { name: "Automated Testing with Selenium", grade: 6 },
+      { name: "Performance Testing", grade: 5 },
+    ],
+  },
+  {
+    id: 16,
+    name: "Peter Martinez",
+    course: "Fullstack Web Programming",
+    subjects: [
+      { name: "JavaScript Fundamentals", grade: 4 },
+      { name: "HTML and CSS Basics", grade: 4 },
+      { name: "Responsive Web Design", grade: 4 },
+    ],
+  },
+  {
+    id: 17,
+    name: "Quinn Nguyen",
+    course: "AWS Cloud Engineering",
+    subjects: [
+      { name: "Introduction to AWS Services", grade: 4 },
+      { name: "Cloud Security", grade: 6 },
+      { name: "Serverless Architecture", grade: 6 },
+    ],
+  },
+  {
+    id: 18,
+    name: "Rachel Turner",
+    course: "Software Testing",
+    subjects: [
+      { name: "Testing Methodologies", grade: 6 },
+      { name: "Automated Testing with Selenium", grade: 3 },
+      { name: "Performance Testing", grade: 4 },
+    ],
+  },
+  {
+    id: 19,
+    name: "Samuel Wright",
+    course: "Fullstack Web Programming",
+    subjects: [
+      { name: "JavaScript Fundamentals", grade: 4 },
+      { name: "HTML and CSS Basics", grade: 5 },
+      { name: "Responsive Web Design", grade: 5 },
+    ],
+  },
+  {
+    id: 20,
+    name: "Tina Adams",
+    course: "AWS Cloud Engineering",
+    subjects: [
+      { name: "Introduction to AWS Services", grade: 6 },
+      { name: "Cloud Security", grade: 5 },
+      { name: "Serverless Architecture", grade: 6 },
+    ],
+  },
+];
+
+console.log(studentsData.length);
