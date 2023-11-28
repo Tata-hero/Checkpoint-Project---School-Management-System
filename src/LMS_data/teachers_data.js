5aa5152601334b8a568db7ffb801132eacd33b34
Tatiana <tata8wheel@gmail.com>
2023-11-28 15:58:45 +0100

    Created teachers data

---
+++ src/LMS_data/teachers_data.js
@@ -0,0 +1,19 @@
+const teachersData = [
+  {
+    id: 1,
+    name: "John Smith",
+    course: "Fullstack Web Programming",
+  },
+  {
+    id: 2,
+    name: "Emily Johnson",
+    course: "AWS Cloud Engineering",
+  },
+  {
+    id: 3,
+    name: "Michael Davis",
+    course: "Software Testing",
+  },
+];
+
+export default teachersData;
