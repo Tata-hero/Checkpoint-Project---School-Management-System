c877b8002f0cba20b6381a09d0b203c4cce7a3fb
Tatiana <tata8wheel@gmail.com>
2023-11-28 16:01:38 +0100

    Created a counter of teachers on the homepage

--- src/components/content.js
+++ src/components/content.js
@@ -1,14 +1,21 @@
 import homePageTemplate from "../pages/home.js";
 import studentsData from "../LMS_data/students_data.js";
+import teachersData from "../LMS_data/teachers_data.js";
 
 function homePage() {
   render(homePageTemplate());
   countStudents();
+  countTeachers();
 }
 
 function countStudents() {
   const studentCounter = document.querySelector(".counter_students");
   studentCounter.innerHTML = studentsData.length.toString();
+}
+
+function countTeachers() {
+  const teachersCounter = document.querySelector(".counter_teachers");
+  teachersCounter.innerHTML = teachersData.length.toString();
 }
 
 function render(content) {
