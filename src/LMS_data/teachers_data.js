da7d44cfe2417f5912ffea8a55953326f37ea245
Tatiana <tata8wheel@gmail.com>
2023-12-07 16:32:09 +0100

    Added description for each teacher and changed id name

--- src/LMS_data/teachers_data.js
+++ src/LMS_data/teachers_data.js
@@ -3,16 +3,22 @@ const teachersData = [
     id: "T1",
     name: "John Smith",
     class: "Fullstack Web Programming",
+    description:
+      "Some quick example text to build on the card title and make up the bulk of the card's content.s",
   },
   {
     id: "T2",
     name: "Emily Johnson",
     class: "AWS Cloud Engineering",
+    description:
+      "Some quick example text to build on the card title and make up the bulk of the card's content.s",
   },
   {
     id: "T3",
     name: "Michael Davis",
     class: "Software Testing",
+    description:
+      "Some quick example text to build on the card title and make up the bulk of the card's content.s",
   },
 ];
 
