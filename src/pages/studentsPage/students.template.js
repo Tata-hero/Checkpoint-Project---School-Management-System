edcef365c45b38994c471a1516e4dca40d2d0496
Tatiana <tata8wheel@gmail.com>
2023-12-12 23:14:24 +0100

    Redesigned the buttons

--- src/pages/studentsPage/students.template.js
+++ src/pages/studentsPage/students.template.js
@@ -8,7 +8,7 @@ function createStudentsPage(studentsCards) {
       ${studentsCards}
     </div>
     <div class="text-center">
-      <a class="mx-auto text-primary" id="addStudentBtn" data-bs-toggle="modal" data-bs-target="#studentForm" style="font-size: 4rem;"><i class="bi bi-plus-circle"></i></a>
+      <a href="#" class="mx-auto text-primary" id="addStudentBtn" data-bs-toggle="modal" data-bs-target="#studentForm" style="font-size: 4rem;"><i class="bi bi-plus-circle"></i></a>
       <div>Add new Student</div>
     </div>
   </div>`;
@@ -19,9 +19,14 @@ function createStudentsCards(student, calculateAverageGrade) {
   <div class="col d-flex justify-content-center">
     <div class="card" style="width: 30rem;">
       <div class="card-body">
-        <div class="row justify-content-end">
-          <a class="col-1 edit-button" data-bs-toggle="modal" data-bs-target="#studentForm" id="${student.id}" data-action="edit" style="font-size: 20px"><i class="bi bi-pencil-square"></i></a>
-          <a class="col-1 delete-button" id="${student.id}" style="font-size: 20px"><i class="bi bi-trash-fill"></i></a>
+        <div class="d-flex gap-2 mb-1 offset-7">
+          <button type="button" class="edit-button btn btn-primary" data-bs-toggle="modal" data-bs-target="#studentForm" id="${student.id}" data-action="edit" ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+          <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"></path>
+          <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"></path>
+          </svg>  Edit</button>
+          <button type="button" class="delete-button btn btn-secondary" id="${student.id}" "><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+          <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0"></path>
+          </svg>  Delete</button>
         </div>
         <h5 class="card-title">${student.name}</h5>
         <h6 class="card-subtitle mb-2 text-body-secondary">${student.class}</h6>
