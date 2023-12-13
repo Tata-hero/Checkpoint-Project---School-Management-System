f44b1b99295d6a73b8b69a0dcee3e67b5412e67c
Tatiana <tata8wheel@gmail.com>
2023-12-13 01:20:16 +0100

    Created classes page functions

---
+++ src/pages/classesPage/classes.components.js
@@ -0,0 +1,165 @@
+import classesData from "../../LMS_data/classes_data.js";
+import classesPageTemplate from "./classes.template.js";
+import classesModalWindow from "../../components/modalWindows/classesModal/classesModal.components.js";
+import pagesContent from "../../components/content.js";
+
+function classesCards() {
+  return updatedClassesData()
+    .map((classItem) => classesPageTemplate.createClassesCards(classItem))
+    .join("");
+}
+
+function displayClassesPage() {
+  return classesPageTemplate.createClassesPage(classesCards());
+}
+
+function addClassModalWindow() {
+  return classesModalWindow.createClassesModalWindow();
+}
+
+function classesModalFunctionality() {
+  return classesModalWindow.setupClassModalFunctionality(
+    handleAddClass,
+    handleEditClass
+  );
+}
+
+if (localStorage.getItem("allClasses") === null) {
+  localStorage.setItem("allClasses", JSON.stringify(classesData));
+}
+
+function handleAddClass() {
+  const { classNameInput, teacherNameInput, classDescriptionInput } =
+    classesModalWindow.getFormElements();
+
+  const className = classNameInput.value;
+  const teacherName = teacherNameInput.value;
+  const classDescription = classDescriptionInput.value;
+
+  const classesFromStorage = getItemFromStorage("allClasses");
+
+  const newClass = {
+    id: (classesFromStorage.length + 1).toString(),
+    className: className,
+    teacherName: teacherName,
+    description: classDescription,
+  };
+
+  if (
+    !classesFromStorage.some((classItem) => classItem.name === newClass.name)
+  ) {
+    classesFromStorage.push(newClass);
+  }
+
+  setItemToStorage("allClasses", classesFromStorage);
+
+  pagesContent.classesPage();
+}
+
+function handleDeleteClassCard() {
+  const classesContainer = document.querySelector(".main");
+
+  classesContainer.addEventListener("click", function (e) {
+    if (
+      e.target.classList.contains("delete_class-button") ||
+      e.target.closest(".delete_class-button")
+    ) {
+      const classId = e.target.id;
+
+      getClassIndexById(classId);
+
+      if (getClassIndexById(classId) !== -1) {
+        const classesFromStorage = getItemFromStorage("allClasses");
+        classesFromStorage.splice(getClassIndexById(classId), 1);
+        setItemToStorage("allClasses", classesFromStorage);
+      }
+
+      pagesContent.classesPage();
+    }
+  });
+}
+
+function displayClassInformation() {
+  const classesContainer = document.querySelector(".main");
+
+  classesContainer.addEventListener("click", function (e) {
+    classesModalWindow.getClassFormInformation(
+      e,
+      getItemFromStorage,
+      getClassIndexById
+    );
+  });
+}
+
+function handleEditClass() {
+  const {
+    submitButton,
+    classNameInput,
+    teacherNameInput,
+    classDescriptionInput,
+  } = classesModalWindow.getFormElements();
+
+  const editedClassName = classNameInput.value;
+  const editedTeacherName = teacherNameInput.value;
+  const editedClassDescription = classDescriptionInput.value;
+
+  const classesFromStorage = getItemFromStorage("allClasses");
+  const editedClassId = parseInt(
+    submitButton.getAttribute("data-class-id"),
+    10
+  );
+
+  const editedClassIndex = getClassIndexById(editedClassId.toString());
+  if (editedClassIndex !== -1) {
+    const editedClass =
+      classesFromStorage[getClassIndexById(editedClassId.toString())];
+    editedClass.name = editedClassName;
+    editedClass.teacher = editedTeacherName;
+    editedClass.classDescription = editedClassDescription;
+
+    setItemToStorage("allClasses", classesFromStorage);
+    pagesContent.classesPage();
+  }
+}
+
+function setCurrentActionForAddBtn() {
+  const addButton = document.querySelector("#addClassBtn");
+
+  addButton.addEventListener("click", function () {
+    classesModalWindow.setCurrentAction("add");
+  });
+}
+
+function getClassIndexById(classId) {
+  const classesFromStorage = getItemFromStorage("allClasses");
+
+  const classIndex = classesFromStorage.findIndex(
+    (classItem) => classItem.id === classId
+  );
+
+  return classIndex;
+}
+
+function updatedClassesData() {
+  const storedClassesData = getItemFromStorage("allClasses") || [];
+  return storedClassesData;
+}
+
+function getItemFromStorage(pKey) {
+  const classesFromStorage = JSON.parse(localStorage.getItem(pKey));
+  return classesFromStorage;
+}
+
+function setItemToStorage(pKey, value) {
+  localStorage.setItem(pKey, JSON.stringify(value));
+}
+
+export default {
+  displayClassesPage,
+  addClassModalWindow,
+  classesModalFunctionality,
+  handleDeleteClassCard,
+  displayClassInformation,
+  updatedClassesData,
+  setCurrentActionForAddBtn,
+};
