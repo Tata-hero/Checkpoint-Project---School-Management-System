5f571658b74c53de2be0f30362208b96710fc722
Tatiana <tata8wheel@gmail.com>
2023-12-13 01:21:22 +0100

    Changed classes data

--- src/LMS_data/classes_data.js
+++ src/LMS_data/classes_data.js
@@ -2,21 +2,21 @@ const classesData = [
   {
     id: "1",
     className: "Fullstack Web Programming",
-    teacher: { id: "T1", name: "John Smith" },
+    teacher: "John Smith",
     description:
       "Some quick example text to build on the card title and make up the bulk of the card's content.s",
   },
   {
     id: "2",
     className: "AWS Cloud Engineering",
-    teacher: { id: "T2", name: "Emily Johnson" },
+    teacher: "Emily Johnson",
     description:
       "Some quick example text to build on the card title and make up the bulk of the card's content.",
   },
   {
     id: "3",
     className: "Software Testing",
-    teacher: { id: "T3", name: "Michael Davis" },
+    teacher: "Michael Davis",
     description:
       "Some quick example text to build on the card title and make up the bulk of the card's content.",
   },
